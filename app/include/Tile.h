998ecbed57d7f38ce5347138cc99eab175bec771
jusa1470 <jusa1470@colorado.edu>
2022-03-07 19:17:02 -0700

    Implemented Tile class (#3)
    * implemented Tile class
    
    * Added the rest of the files

--- app/include/Tile.h
+++ app/include/Tile.h
@@ -1,3 +1,25 @@
-class Tile {
+#include <tuple>
+#include <SDLSetup.h>
 
+#ifndef _TILE_H
+#define _TILE_H
+
+class Tile {
+    public:
+        Tile(int dots_, int value_);
+        ~Tile(){};
+        void setDots(int dots_);
+        int getDots();
+        void setValue(int value_);
+        int getValue();
+        void setLocation(int i, int j);
+        std::tuple<int, int> getLocation();
+
+    private:
+        int dots;
+        int value;
+        std::tuple<int, int> location;
+        SDLSetup* sdlSetup;
 };
+
+#endif
--- app/src/Tile.cpp
+++ app/src/Tile.cpp
@@ -1,1 +1,31 @@
 #include <Tile.h>
+
+Tile::Tile(int dots_, int value_) {
+    this->dots = dots_;
+    this->value = value_;
+    this->location = std::tuple<int, int>{-1, -1};
+}
+
+void Tile::setDots(int dots_) {
+    this->dots = dots_;
+}
+
+int Tile::getDots() {
+    return dots;
+}
+
+void Tile::setValue(int value_) {
+    this->value = value_;
+}
+
+int Tile::getValue() {
+    return value;
+}
+
+void Tile::setLocation(int i, int j) {
+    this->location = std::tuple<int, int>{i, j};
+}
+
+std::tuple<int, int> Tile::getLocation() {
+    return location;
+}
